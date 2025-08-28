864f5c2e90ed3a452504468d44d83b9ed5067fd1
Hamzeh <h.qorbani7@gmail.com>
2025-08-28 10:48:11 +0330

    create class model

---
+++ models/class_model.py
@@ -0,0 +1,15 @@
+class ClassModel:
+    def __init__(self, db):
+        self.db = db
+
+    def add_class(self, name, capacity):
+        self.db.execute("INSERT INTO classes (name, capacity) VALUES (?, ?)", (name, capacity))
+
+    def get_all_classes(self):
+        return self.db.fetchall("SELECT id, name, capacity FROM classes")
+
+    def update_class(self, class_id, name, capacity):
+        self.db.execute("UPDATE classes SET name = ?, capacity = ? WHERE id = ?", (name, capacity, class_id))
+
+    def delete_class(self, class_id):
+        self.db.execute("DELETE FROM classes WHERE id = ?", (class_id,))
