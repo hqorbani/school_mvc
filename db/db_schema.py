d7f26c71a0772d43e83b0ece02a58501331655a7
Hamzeh <h.qorbani7@gmail.com>
2025-08-28 10:47:34 +0330

    create tables

---
+++ db/db_schema.py
@@ -0,0 +1,26 @@
+def create_tables(db):
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS classes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            capacity INTEGER NOT NULL CHECK(capacity > 0)
+        )
+    """)
+
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        )
+    """)
+
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS enrollments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            student_id INTEGER NOT NULL,
+            class_id INTEGER NOT NULL,
+            FOREIGN KEY(student_id) REFERENCES students(id),
+            FOREIGN KEY(class_id) REFERENCES classes(id),
+            UNIQUE(student_id, class_id)
+        )
+    """)
