c9e20764a722884e8a3e129e61e4bad79bcdb05e
Hamzeh <h.qorbani7@gmail.com>
2025-08-28 10:51:34 +0330

    create main file

---
+++ main.py
@@ -0,0 +1,22 @@
+from ttkbootstrap import Window
+from db.db_connection import Database
+from db.db_schema import create_tables
+from models.class_model import ClassModel
+from controllers.class_controller import ClassController
+from views.class_view import ClassView
+
+def main():
+    db = Database()
+    create_tables(db)
+
+    class_model = ClassModel(db)
+    class_controller = ClassController(class_model)
+
+    app = Window(themename="flatly")
+    ClassView(app, class_controller)
+    app.mainloop()
+
+    db.close()
+
+if __name__ == "__main__":
+    main()
