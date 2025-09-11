3cc0e169a8f9c707e3621d726cb1cd3d651b24f6
Hamzeh <h.qorbani7@gmail.com>
2025-09-11 16:35:05 +0330

    add new fields

--- models/student_model.py
+++ models/student_model.py
@@ -2,11 +2,12 @@ class StudentModel:
     def __init__(self, db):
         self.db = db
 
-    def add_student(self, name):
-        self.db.execute("INSERT INTO students (name) VALUES (?)", (name,))
+    def add_student(self, name , national_code , mobile , password):
+        self.db.execute("INSERT INTO students (name, national_code , mobile , password) VALUES (?,?,?,?)"
+                        , (name, national_code , mobile , password))
 
     def get_all_students(self):
-        return self.db.fetchall("SELECT id, name FROM students")
+        return self.db.fetchall("SELECT id, name, national_code , mobile FROM students")
 
     def delete_student(self, student_id):
         self.db.execute("DELETE FROM students WHERE id = ?", (student_id,))
