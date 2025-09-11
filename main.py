3df70860f2379bdd46dcbc8c303e07ceb6bb0ec8
Hamzeh <h.qorbani7@gmail.com>
2025-09-11 15:04:22 +0330

    update main by views and controllers

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-from ttkbootstrap import Window
-from db.db_connection import Database
-from db.db_schema import create_tables
-from models.class_model import ClassModel
-from controllers.class_controller import ClassController
-from views.class_view import ClassView
+from views.main_window import MainWindow
 
 def main():
-    db = Database()
-    create_tables(db)
-
-    class_model = ClassModel(db)
-    class_controller = ClassController(class_model)
-
-    app = Window(themename="darkly")
-    ClassView(app, class_controller)
+    app = MainWindow()
     app.mainloop()
-
-    db.close()
 
 # if __name__ == "__main__":
 main()
