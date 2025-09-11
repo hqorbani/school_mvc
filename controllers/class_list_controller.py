cc1d3699c2e9e339adae31635d63f6d9e0bcbb5e
Hamzeh <h.qorbani7@gmail.com>
2025-09-11 15:12:11 +0330

    rename method name

--- controllers/class_list_controller.py
+++ controllers/class_list_controller.py
@@ -2,8 +2,8 @@ class ClassListController:
     def __init__(self, model, view):
         self.model = model
         self.view = view
-        self.load_students()
+        self.load_class_list()
 
-    def load_students(self):
+    def load_class_list(self):
         students = self.model.get_all_classes()
         self.view.populate(students)
