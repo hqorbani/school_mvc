caa37a7fdc8bd93d8674577543519dccf573bf5c
Hamzeh <h.qorbani7@gmail.com>
2025-09-25 11:03:11 +0330

    udpdate for cursor and fetchon method

--- db/db_connection.py
+++ db/db_connection.py
@@ -3,12 +3,17 @@ import sqlite3
 class Database:
     def __init__(self, db_path="database.db"):
         self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor() 
 
     def execute(self, query, params=()):
         cursor = self.conn.execute(query, params)
         self.conn.commit()
         return cursor
-
+    
+    def fetchone(self, query, params=()):
+        self.cursor.execute(query, params)
+        return self.cursor.fetchone()
+    
     def fetchall(self, query, params=()):
         return self.conn.execute(query, params).fetchall()
 
