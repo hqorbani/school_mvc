34919334fdeaedaa25bfc7708e927e163ba8c234
Hamzeh <h.qorbani7@gmail.com>
2025-09-29 09:34:59 +0330

    update SuccessMessages class

--- messages/successes.py
+++ messages/successes.py
@@ -1,3 +1,4 @@
 class SuccessMessages:
     SUCCESS_STUDENT_CLASS_REGISTRATION = "ثبت کلاس برای دانش آموز با موفقیت انجام شد."
     SUCCESS_CLASS_REGISTRATION = "ثبت  کلاس با موفقیت انجام شد."
+    SUCCESS_STUDENT_REGISTRATION = "ثبت  دانش آموز با موفقیت انجام شد."
