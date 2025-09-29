cc03d968810f01155f31a7d3354d9e91bea15bcc
Hamzeh <h.qorbani7@gmail.com>
2025-09-29 09:29:39 +0330

    update error messages

--- messages/errors.py
+++ messages/errors.py
@@ -3,4 +3,7 @@ class ErrorMessages:
     INVALID_CAPACITY = "ظرفیت کلاس باید عددی مثبت باشد."
     CLASS_NOT_FOUND = "کلاسی با این شناسه یافت نشد."
     STUDENT_NOT_FOUND = "دانش‌آموزی با این شناسه یافت نشد."
+    STUDENT_NO_SELECTED = "دانش‌آموزی انتخاب نشده است."
     DUPLICATE_ENROLLMENT = "دانش‌آموز قبلاً در این کلاس ثبت‌نام شده است."
+    ERROR_STUDENT_CLASS_REGISTRATION = "ثبت کلاس برای دانش آموز با خطا روبر شد. لطفا مساله را با مدیر سیستم مطرح کنید"
+    ERROR_CLASS_REGISTRATION = "ثبت کلاس با خطا روبر شد. لطفا مساله را با مدیر سیستم مطرح کنید"
